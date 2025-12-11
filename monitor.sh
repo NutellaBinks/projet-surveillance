b5c8b076fedd36046ba6f6c4cf733a4b13811ce1
Nutellalpb <sionnathanperso@gmail.com>
2025-12-11 10:56:31 +0100

    Feat: Ajout option -help et fichier .gitignore

--- monitor.sh
+++ monitor.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+if [[ "$1" == "-help" ]]; then
+    echo "Usage : ./monitor.sh [OPTION]"
+    echo "Script de surveillance système (CPU, RAM, Disque)"
+    echo ""
+    echo "Options :"
+    echo "  -help    Affiche ce message d'aide"
+    exit 0
+fi
 echo "--- Utilisation CPU ---"
 top -n 1 | grep "Cpu"
 echo "--- Utilisation RAM ---"
